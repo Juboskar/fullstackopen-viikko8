45029662c70f35acbf9a0ff2ca76f94b659e138e
Juboskar <oskari.juvakka@gmail.com>
2022-07-17 23:30:09 +0300

    8.15

--- library-backend/graphql/resolvers.js
+++ library-backend/graphql/resolvers.js
@@ -4,19 +4,44 @@ const Author = require('../models/author');
 const resolvers = {
   Mutation: {
     addBook: async (root, args) => {
-      let author = await Author.findOne({ name: args.author });
+      const author = await Author.findOne({ name: args.author });
+
       if (!author) {
-        author = await new Author({ name: args.author });
-        author.save();
+        try {
+          new Author({ name: args.author }).save();
+        } catch (error) {
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          });
+        }
       }
-      const book = new Book({ ...args, author: author.id });
-      return (await book.save()).populate('author');
+
+      try {
+        return (await new Book({ ...args, author: author.id }).save()).populate(
+          'author'
+        );
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        });
+      }
     },
     editAuthor: async (root, args) => {
       const author = await Author.findOne({ name: args.name });
+
       if (!author) return null;
+
       author.born = args.setBornTo;
-      return author.save();
+
+      try {
+        author.save();
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        });
+      }
+
+      return author;
     },
   },
   Query: {
